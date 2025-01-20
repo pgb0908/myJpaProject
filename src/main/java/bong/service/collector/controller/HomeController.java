f1933500ea72c77e121c3b2f06ef2ba693a5ebd3
gyubong_park <pgb8909@naver.com>
2025-01-20 23:33:58 +0900

    feat) 화면 추가

---
+++ src/main/java/bong/service/collector/controller/HomeController.java
@@ -0,0 +1,20 @@
+package bong.service.collector.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        Boolean hasSession = false;
+
+        if(!hasSession){
+            return "login";
+        }
+
+        return "index";
+    }
+}
