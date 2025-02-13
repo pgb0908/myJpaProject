e61c221f8796c0b1706a65c614ffd1a41a5bccd5
gyubong_park <gyubong_park@tmaxsoft.co.kr>
2025-02-13 18:26:12 +0900

    feat) 직접 제작한 login 화면으로 로그인 가능하도록 수정

--- src/main/java/bong/service/collector/config/SecurityConfig.java
+++ src/main/java/bong/service/collector/config/SecurityConfig.java
@@ -1,16 +1,28 @@
 package bong.service.collector.config;
 
+import bong.service.collector.service.SecurityService;
 import jakarta.servlet.DispatcherType;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     // https://dnl1029.tistory.com/35
@@ -44,4 +56,5 @@ public class SecurityConfig {
 
         return http.build();
     }
+
 }
