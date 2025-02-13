fac613cccce7bff4db42435d90cacf9c48e9cddf
gyubong_park <pgb8909@naver.com>
2025-02-14 01:39:09 +0900

    feat) users 정보 자동 추가 및 삭제

--- src/main/java/bong/service/collector/service/UserInfoCleaner.java
+++ src/main/java/bong/service/collector/service/UserInfoCleaner.java
@@ -1,13 +1,14 @@
-package bong.service.collector.component;
+package bong.service.collector.service;
 
 import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @Slf4j
-@Component
+@Service
 public class UserInfoCleaner {
 
     @Autowired
